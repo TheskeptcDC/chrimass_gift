eafdb0fe1beb856b2aa0f7e5cdfa408221604d7f
daliso banda <bandadaliso255@gmail.com>
2024-12-26 19:38:25 +0200

    fixed the side bar controller

--- controllers/side_bar_category_controller.php
+++ controllers/side_bar_category_controller.php
@@ -8,12 +8,17 @@ $categories = $category->select_active_category_names($conn);
     if ($categories == "DB returned no results") {
         echo "no cats";
     } else {
-        $number_of_categories = count($categories) - 1;
-        while ($number_of_categories > 0) {
-            echo '<li class="list-group-item"><a href="index.php?action=category&name=' . $categories[$number_of_categories] . '">' . $categories[$number_of_categories] . '</a></li>';
+        $number_of_categories = count($categories);
+        $count = 0;
+        while ($number_of_categories >= 0) {
 
             $number_of_categories = $number_of_categories - 1;
+            echo '<li class="list-group-item"><a href="index.php?action=category&name=' . $categories[$count] . '">' . $categories[$count] . '</a></li>';
+            $number_of_categories = $number_of_categories - 1;
+            $count ++;
         }
+        
+     
     }
     
 ?>
